aa73c42ba6a9979eb5cab81a722c155c4ec5d87f
EskoJTH <esjutaha@student.jyu.fi>
2016-09-07 22:52:35 +0300

    Autolaskuri TIEA212
    Menejo

---
+++ Autolaskuri/Autolaskuri/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autolaskuri
+{
+
+    public partial class Autolaskuri : Form
+    {
+        private Bitmap bike = new Bitmap(Properties.Resources.bike);
+        private Bitmap car = new Bitmap(Properties.Resources.car);
+
+        int henkiloAutoLkm;
+        int polkupyoratLkm;
+
+        private int henkiloautot
+        {
+            get { return henkiloAutoLkm; }
+            set { henkiloAutoLkm = value;
+                Kuva auto = new Kuva(car);
+                auto.BringToFront();
+                this.Controls.Add(auto);
+            }
+        }
+        private int polkupyorat
+        {
+            get { return polkupyoratLkm; }
+            set { polkupyoratLkm = value;
+                Kuva pyora = new Kuva(bike);
+                pyora.BringToFront();
+                this.Controls.Add(pyora);
+            }
+        }
+
+        public Autolaskuri()
+        {
+            InitializeComponent();
+        }
+
+        private void henkiloautoClick(object sender, EventArgs e)
+        {
+            henkiloautot++;
+            HenkiloautoLabel.Text=henkiloautot.ToString();
+            
+
+        }
+
+        private void PolkupyoraClick(object sender, EventArgs e)
+        {
+            polkupyorat++;
+            polkupyoraLabel.Text = polkupyorat.ToString();
+        }
+
+        private void AutoBoxClick(object sender, EventArgs e)
+        {
+            AboutBox1 aboutbox = new AboutBox1();
+            aboutbox.ShowDialog();
+        }
+
+        private void Vari(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            Color color = colorDialog1.Color;
+            this.BackColor = color;
+
+        }
+
+        private void UusiIkkunaClick(object sender, EventArgs e)
+        {
+            Autolaskuri autolaskuri = new Autolaskuri();
+            autolaskuri.ShowDialog();
+        }
+
+    }
+}
---
+++ Autolaskuri/Autolaskuri/Kuva.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autolaskuri
+{
+    class Kuva : PictureBox
+    {
+        private int nopeus = 5;
+        private Timer ajastin = new Timer();
+        private int parentWidth;
+
+        public Kuva(Image image)
+        {
+            this.Image = image;
+            this.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.ParentChanged += new EventHandler(this.OnParentChanged);
+            this.Visible = true;
+
+            ajastin.Enabled = true;
+            ajastin.Tick += new EventHandler(this.AjastinTick);
+        }
+
+        private void OnParentChanged(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            this.parentWidth = this.Parent.Size.Width;
+            int y = random.Next(0, this.Parent.Size.Height-Image.Height);
+            int x = random.Next(0, this.Parent.Size.Width - Image.Width);
+            System.Drawing.Point paikka = new System.Drawing.Point(x, y);
+                this.Location = paikka;
+        }
+
+        private void AjastinTick(object sender, EventArgs e)
+        {
+            if (this.Left + this.Size.Width + nopeus > this.parentWidth)
+                nopeus = -nopeus;
+            if (this.Left + nopeus < 0)
+                nopeus = -nopeus;
+            this.Left += nopeus;
+        }
+    }
+}
