0b774617058f16103dee67ca1992808b33affd14
EskoJTH <Esjutaha@student.jyu.fi>
2016-11-10 03:05:15 +0200

    haijjaaa

---
+++ TIEA212Harjoitustyo4/Taso1/KilometriLaskuri/KilometriLaskuri/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Treeni_WPF_komponentti;
+
+namespace KilometriLaskuri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void NewCmdExecuted(object s, ExecutedRoutedEventArgs e)
+        {
+            UserControl1 ikkuna = new UserControl1();
+            this.LajitteluPaneeli.Children.Add(ikkuna);
+        }
+        void NewCmdCanExecute(object s, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+            void CloseCmdExecuted(object s, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        void CloseCmdCanExecute(object s, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
