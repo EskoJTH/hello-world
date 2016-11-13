4d987b748c8a0d58e45a647b7cc6f0c9c3aac5be
EskoJTH <Esjutaha@student.jyu.fi>
2016-11-13 22:41:41 +0200

    taso 2 mutafakas

---
+++ TIEA212Harjoitustyo5/Taso2/KirjainClickeri/KirjainClickeri/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KirjainClickeri
+{
+    public class Henkilo
+    {
+        private string etunimi;
+        private string sukunimi;
+        private int ika = 11;
+        private string kansallisuus;
+        private static Random r = new Random(2016);
+        public Henkilo()
+        {
+            string[] etunimet = { "John", "Jane", "Jack", "Jill", "Jules", "Jake", "Julian", "Jonathan", "Julia", "Judith" };
+            string[] sukunimet = { "Doe", "Smith", "Jones", "Taylor", "Brown", "Wilson", "Davis", "Johnson", "Robinson" };
+            string[] kansat = { "USA", "Kanada", "Australia" };
+            Ika = r.Next(1, 110);
+            Kansallisuus = kansat[r.Next(0, kansat.Length)];
+            Etunimi = etunimet[r.Next(0, etunimet.Length)];
+            Sukunimi = sukunimet[r.Next(0, sukunimet.Length)];
+        }
+
+        public string Kansallisuus
+        {
+            get { return kansallisuus; }
+            set { kansallisuus = value; }
+        }
+
+        public string Etunimi
+        {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
+
+        public string Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
+
+        public int Ika
+        {
+            get { return ika; }
+            set { if (value > 0 && value <= 110) ika = value; }
+        }
+
+
+
+    }
+
+    public partial class MainWindow : Window
+    {
+        private List<Henkilo> henkilot;
+
+        public List<Henkilo> Henkilot
+        {
+            get { return henkilot; }
+            set { henkilot = value; }
+        }
+        public MainWindow()
+        {
+            henkilot = new List<Henkilo>();
+            for (int i = 0; i < 32; i++)
+            {
+                henkilot.Add(new Henkilo());
+            }
+            InitializeComponent();
+        }
+    }
+
+    [ValueConversion(typeof(string), typeof(Color))]
+    public class CountryColorConverter : IValueConverter
+    {
+
+        // muunnos bool-tyypistä visibility-tyyppiin
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string arvo = (string)value;
+            Color keltainen = Color.FromRgb(255, 255, 0);
+            Color VSininen = Color.FromRgb(100,100, 255);
+            Color TSininen = Color.FromRgb(0, 0, 255);
+            if (arvo.Equals("USA")) return keltainen;
+            if (arvo.Equals("Kanada")) return VSininen;
+            if (arvo.Equals("Englanti")) return TSininen;
+            return Color.FromRgb(0, 0, 0);
+        }
+
+        // muunnos visibility-tyypistä bool-tyyppiin
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            Color arvo = (Color)value;
+            if (arvo.Equals(Color.FromRgb(255, 255, 0))) return "USA";
+            if (arvo.Equals(Color.FromRgb(100, 100, 255))) return "Kanada";
+            if (arvo.Equals(Color.FromRgb(0, 0, 255))) return "Englanti";
+            return "Yarr Harr Pirates";
+        }
+    }
+}
