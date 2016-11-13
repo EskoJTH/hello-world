4c7a7ed48ea002bb99065a71cbfe69a92b3b7528
EskoJTH <Esjutaha@student.jyu.fi>
2016-11-13 23:50:30 +0200

    taso 3 aloitettu

--- TIEA212Harjoitustyo5/Taso3/RistiNolla/RistiNolla/MainWindow.xaml.cs
+++ TIEA212Harjoitustyo5/Taso3/RistiNolla/RistiNolla/MainWindow.xaml.cs
@@ -36,89 +36,89 @@ namespace RistiNolla
 
             InitializeComponent();
 
+        }
+    }
 
+    public class Henkilo
+    {
+        private string etunimi;
+        private string sukunimi;
+        private int ika = 11;
+        private string kansallisuus;
+        private static Random r = new Random(2016);
+        public Henkilo()
+        {
+            string[] etunimet = { "John", "Jane", "Jack", "Jill", "Jules", "Jake", "Julian", "Jonathan", "Julia", "Judith" };
+            string[] sukunimet = { "Doe", "Smith", "Jones", "Taylor", "Brown", "Wilson", "Davis", "Johnson", "Robinson" };
+            string[] kansat = { "USA", "Kanada", "Australia" };
+            Ika = r.Next(1, 110);
+            Kansallisuus = kansat[r.Next(0, kansat.Length)];
+            Etunimi = etunimet[r.Next(0, etunimet.Length)];
+            Sukunimi = sukunimet[r.Next(0, sukunimet.Length)];
         }
 
-        public class Henkilo
+        public string Kansallisuus
         {
-            private string etunimi;
-            private string sukunimi;
-            private int ika = 11;
-            private string kansallisuus;
-            private static Random r = new Random(2016);
-            public Henkilo()
-            {
-                string[] etunimet = { "John", "Jane", "Jack", "Jill", "Jules", "Jake", "Julian", "Jonathan", "Julia", "Judith" };
-                string[] sukunimet = { "Doe", "Smith", "Jones", "Taylor", "Brown", "Wilson", "Davis", "Johnson", "Robinson" };
-                string[] kansat = { "USA", "Kanada", "Australia" };
-                Ika = r.Next(1, 110);
-                Kansallisuus = kansat[r.Next(0, kansat.Length)];
-                Etunimi = etunimet[r.Next(0, etunimet.Length)];
-                Sukunimi = sukunimet[r.Next(0, sukunimet.Length)];
-            }
-
-            public string Kansallisuus
-            {
-                get { return kansallisuus; }
-                set { kansallisuus = value; }
-            }
-
-            public string Etunimi
-            {
-                get { return etunimi; }
-                set { etunimi = value; }
-            }
-
-            public string Sukunimi
-            {
-                get { return sukunimi; }
-                set { sukunimi = value; }
-            }
-
-            public int Ika
-            {
-                get { return ika; }
-                set { if (value > 0 && value <= 110) ika = value; }
-            }
-
-
-
+            get { return kansallisuus; }
+            set { kansallisuus = value; }
         }
 
-        [ValueConversion(typeof(string), typeof(SolidColorBrush))]
-        public class CountryColorConverter : IValueConverter
+        public string Etunimi
         {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
 
-            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                string arvo = (string)value;
-                Color keltainen = Color.FromRgb(255, 255, 0);
-                Color VSininen = Color.FromRgb(100, 100, 255);
-                Color TSininen = Color.FromRgb(0, 0, 255);
-                Color Oranssi = Color.FromRgb(255, 100, 100);
-                Color Musta = Color.FromRgb(0, 0, 0);
-                SolidColorBrush pikachu = new SolidColorBrush(keltainen);
-                SolidColorBrush squirtle = new SolidColorBrush(VSininen);
-                SolidColorBrush blastoise = new SolidColorBrush(TSininen);
-                SolidColorBrush charizard = new SolidColorBrush(Oranssi);
-                SolidColorBrush mewtoo = new SolidColorBrush(Oranssi);
-                if (arvo.Equals("USA")) return pikachu;
-                if (arvo.Equals("Kanada")) return squirtle;
-                if (arvo.Equals("Englanti")) return blastoise;
-                if (arvo.Equals("Australia")) return charizard;
-                return mewtoo;
-            }
+        public string Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
 
-            public object ConvertBack(object value, Type targetType,
-                object parameter, CultureInfo culture)
-            {
-                Color arvo = (Color)value;
-                if (arvo.Equals(Color.FromRgb(255, 255, 0))) return "USA";
-                if (arvo.Equals(Color.FromRgb(100, 100, 255))) return "Kanada";
-                if (arvo.Equals(Color.FromRgb(0, 0, 255))) return "Englanti";
-                if (arvo.Equals(Color.FromRgb(255, 100, 100))) return "Australia";
-                return "Yarr Harr Pirates";
-            }
+        public int Ika
+        {
+            get { return ika; }
+            set { if (value > 0 && value <= 110) ika = value; }
+        }
+
+
+
+    }
+
+    [ValueConversion(typeof(string), typeof(SolidColorBrush))]
+    public class CountryColorConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string arvo = (string)value;
+            Color keltainen = Color.FromRgb(255, 255, 0);
+            Color VSininen = Color.FromRgb(100, 100, 255);
+            Color TSininen = Color.FromRgb(0, 0, 255);
+            Color Oranssi = Color.FromRgb(255, 100, 100);
+            Color Musta = Color.FromRgb(0, 0, 0);
+            SolidColorBrush pikachu = new SolidColorBrush(keltainen);
+            SolidColorBrush squirtle = new SolidColorBrush(VSininen);
+            SolidColorBrush blastoise = new SolidColorBrush(TSininen);
+            SolidColorBrush charizard = new SolidColorBrush(Oranssi);
+            SolidColorBrush mewtoo = new SolidColorBrush(Oranssi);
+            if (arvo.Equals("USA")) return pikachu;
+            if (arvo.Equals("Kanada")) return squirtle;
+            if (arvo.Equals("Englanti")) return blastoise;
+            if (arvo.Equals("Australia")) return charizard;
+            return mewtoo;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            Color arvo = (Color)value;
+            if (arvo.Equals(Color.FromRgb(255, 255, 0))) return "USA";
+            if (arvo.Equals(Color.FromRgb(100, 100, 255))) return "Kanada";
+            if (arvo.Equals(Color.FromRgb(0, 0, 255))) return "Englanti";
+            if (arvo.Equals(Color.FromRgb(255, 100, 100))) return "Australia";
+            return "Yarr Harr Pirates";
         }
     }
 }
+
