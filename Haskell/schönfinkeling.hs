0ba2c94610ffd697e045641897b32a9478bac24e
EskoJTH <esjutaha@student.jyu.fi>
2017-01-31 14:56:04 +0200

    Lisää haskell hommia

--- Haskell/schönfinkeling.hs
+++ Haskell/schönfinkeling.hs
@@ -1,5 +1,8 @@
 module Schanfinkeling where
 
+schanfinkeling :: ((a,b)->c)->a->b->c
 schanfinkeling f x y = f (x, y)
 
+unSchanfinkeling :: (a->b->c)->(a,b)->c
 unSchanfinkeling f (x, y) = f x y
+
---
+++ Haskell/vali.hs
@@ -0,0 +1,5 @@
+testi :: Float->Float->Float
+testi x c = x + increment c
+
+increment :: Float->Float
+increment a = a**2
