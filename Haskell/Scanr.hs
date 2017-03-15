cb025c2f980ad0130954ed812cd03385658e0f93
EskoJTH <esjutaha@student.jyu.fi>
2017-03-15 16:21:03 +0200

    tein Unfoldin

--- Haskell/Scanr.hs
+++ Haskell/Scanr.hs
@@ -1,3 +1,4 @@
+{-#LANGUAGE ScopedTypeVariables#-}
 module Scanr where
 
 myScanr :: (a -> b -> b) -> b -> [a] -> [b]
@@ -7,5 +8,8 @@ myScanl :: (a -> b -> b) -> b -> [a] -> [b]
 myScanl fun empty = g . foldr f e
   where
     g h = empty : h empty
-    f x acc k = k fun x: acc (k fun x)
-    e = (const empty)
+    f x acc k = (fun k x): acc (fun k x)
+    e = (const [])
+    
+--Saan tuosta f:st‰ jostain syyst‰ virheilmoituksen. En yht‰‰n keksi ett‰ miksi.
+--Virhe n‰ytt‰‰ t‰lt‰
