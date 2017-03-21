d4f065db8a7835abf613758718fd3c454a8ac68d
EskoJTH <esjutaha@student.jyu.fi>
2017-03-21 18:10:00 +0200

    Asteroideja viiuuuh viiuuh viuh

--- Haskell/Asteroids/Asteroids.hs
+++ Haskell/Asteroids/Asteroids.hs
@@ -1,13 +1,15 @@
 
 
 module Main where
+import Control.Monad (join)
+import Data.Bifunctor (bimap)
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Interface.Pure.Simulate
 import Graphics.Gloss.Interface.Pure.Display
 import Graphics.Gloss.Data.Picture
 
-data AsteroidWorld = Play [Rock] Ship [Bullet]
+data AsteroidWorld = Play [Rock] Ship UFO [Bullet]
                    | GameOver 
                    deriving (Eq,Show)
 
@@ -17,43 +19,55 @@ type Age          = Float
 
 
 
-data Ship   = Ship   PointInSpace Velocity      
+data Ship   = Ship   PointInSpace Velocity
     deriving (Eq,Show)
-data Bullet = Bullet PointInSpace Velocity Age  
+data Bullet = Bullet PointInSpace Velocity Age
     deriving (Eq,Show)
-data Rock   = Rock   PointInSpace Size Velocity 
+data Rock   = Rock   PointInSpace Size Velocity
+    deriving (Eq,Show)
+data UFO   = UFO PointInSpace Size  Velocity
     deriving (Eq,Show)
 
 initialWorld :: AsteroidWorld
 initialWorld = Play
-                   [Rock (150,150)  45 (2,6)    
-                   ,Rock (-45,201)  45 (13,-8) 
-                   ,Rock (45,22)    25 (-2,8)  
-                   ,Rock (-210,-15) 30 (-2,-8) 
-                   ,Rock (-45,-201) 25 (8,2)   
-                   ] -- The default rocks
-                   (Ship (0,0) (0,5)) -- The initial ship
-                   [] -- The initial bullets (none)
+                   [Rock (150,150)  45 (2,6)
+                   ,Rock (-45,201)  45 (13,-8)
+                   ,Rock (45,22)    25 (-2,8)
+                   ,Rock (-210,-15) 30 (-2,-8)
+                   ,Rock (-45,-201) 25 (8,2) 
+                   ] 
+                   (Ship (0,0) (0,5)) 
+                   (UFO (40,50) 25 (13,45))--My UFO
+                   [] 
 
 
 simulateWorld :: Float -> (AsteroidWorld -> AsteroidWorld)
 
-simulateWorld _ GameOver = GameOver  
+simulateWorld _ GameOver = GameOver
 
-simulateWorld timeStep (Play rocks (Ship shipPos shipV) bullets) 
+simulateWorld timeStep (Play rocks (Ship shipPos shipV) ufo  bullets)
   | any (collidesWith shipPos) rocks = GameOver
-  | otherwise = Play (concatMap updateRock rocks) 
-                              (Ship newShipPos shipV)
-                              (concat (map updateBullet bullets))
+  | otherwise = Play (concatMap updateRock rocks)
+                (Ship newShipPos shipV)
+                (updateUFO ufo)
+                (concat (map updateBullet bullets))
   where
       collidesWith :: PointInSpace -> Rock -> Bool
-      collidesWith p (Rock rp s _) 
-       = magV (rp .- p) < s 
+      collidesWith p (Rock rp s _)
+       = magV (rp .- p) < s
+
+      collidesWithU :: PointInSpace -> UFO -> Bool
+      collidesWithU p (UFO rp s _)
+       = magV (rp .- p) < s
 
       collidesWithBullet :: Rock -> Bool
-      collidesWithBullet r 
-       = any (\(Bullet bp _ _) -> collidesWith bp r) bullets 
-     
+      collidesWithBullet r
+       = any (\(Bullet bp _ _) -> collidesWith bp r) bullets
+
+      collidesWithUFO :: UFO -> Bool
+      collidesWithUFO r
+       = any (\(Bullet bp _ _) -> collidesWithU bp r) bullets
+         
       updateRock :: Rock -> [Rock]
       updateRock r@(Rock p s v) 
        | collidesWithBullet r && s < 7 
@@ -62,6 +76,13 @@ simulateWorld timeStep (Play rocks (Ship shipPos shipV) bullets)
             = splitRock r
        | otherwise                     
             = [Rock (restoreToScreen (p .+ timeStep .* v)) s v]
+
+      updateUFO :: UFO -> UFO
+      updateUFO r@(UFO p s v)
+       | collidesWithUFO r
+            = UFO (restoreToScreen (p .+ timeStep .* join bimap (negate) v)) s (join bimap (negate) v) 
+       | otherwise                     
+            = UFO (restoreToScreen (p .+ timeStep .* v)) s v
  
       updateBullet :: Bullet -> [Bullet] 
       updateBullet (Bullet p v a) 
@@ -98,14 +119,15 @@ drawWorld _ _ _ GameOver
      . text 
      $ "Game Over!"
 
-drawWorld rocket rock bullet (Play rocks (Ship (x,y) (vx,vy)) bullets) 
-  = pictures [ship, asteroids,shots]
-   where 
+drawWorld rocket rock bullet (Play rocks (Ship (x,y) (vx,vy)) (UFO (p1,p2) s v) bullets)
+  = pictures [ship, asteroids,shots,ufo]
+   where
     ship      = pictures [translate x y (rocket)]
-    asteroids = pictures [translate x y (asteroids)
+    asteroids = pictures [translate x y (rock)
                          | Rock   (x,y) s _ <- rocks]
-    shots     = pictures [translate x y ((bullet)) 
+    shots     = pictures [translate x y ((bullet))
                          | Bullet (x,y) _ _ <- bullets]
+    ufo       = pictures [translate p1 p2 (rocket)]
 
 
 
@@ -118,9 +140,9 @@ handleEvents (EventKey (MouseButton LeftButton) Down _ clickPos)
 handleEvents _ GameOver = GameOver
 
 handleEvents (EventKey (MouseButton LeftButton) Down _ clickPos)
-             (Play rocks (Ship shipPos shipVel) bullets)
+             (Play rocks (Ship shipPos shipVel) ufo bullets)
              = Play rocks (Ship shipPos newVel) 
-                          (newBullet : bullets)
+                          ufo (newBullet : bullets)
  where 
      newBullet = Bullet shipPos 
                         (-150 .* norm (shipPos .- clickPos)) 
@@ -162,9 +184,14 @@ main =
   loadBMP "Rock1.bmp" >>= \rock ->
   play 
          (InWindow "Asteroids!" (550,550) (20,20)) 
-         black 
-         24 
+         black
+         24
          initialWorld 
          (drawWorld rocket rock bullet)
          handleEvents
          simulateWorld
+
+     -- do pictures [rocket, ...]
+     -- do rocket <- ask
+     --    pictures [rocket, ...]
+
