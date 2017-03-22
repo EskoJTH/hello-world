a244003eaaed69e017f05d0f6824a4b072b99727
EskoJTH <esjutaha@student.jyu.fi>
2017-03-22 15:55:19 +0200

    lisäsin tuotatuota scotty web toimii perust taasolla.

--- Haskell/BasicIO.hs
+++ Haskell/BasicIO.hs
@@ -2,39 +2,40 @@
 main = do
   --Do block starts here
   
-    --IO operation
-    putStr   "Pick a natural number less than 10,"
-    --IO operation
-    putStrLn "and I'll try to guess what it is!"
+    --plain action --IO operation
+    putStr   "Pick a natural number less than 10," :: IO ()
+    --plain action --IO operation
+    putStrLn "and I'll try to guess what it is!" :: IO ()
 
     --a let block that defines a funktion called "loop" starts here
-    --The let portion is inside the do block but IO actions aren't executed iside it
+    --Bind --The let portion is inside the do block but IO actions aren't executed iside it
     let loop :: Int -> Int -> IO ()
         loop a b 
          | a==b      = putStrLn ("The number is "++show a) -- this is an IO operation that gets executed later once it is called from a do block.
          | otherwise = do
                         -- Another do block
              
-                        --another let block type of try is Int
-                        let try = (a + b) `div` 2
+                        --Bind --another let block type of try is Int
+                        let try = (a + b) `div` 2 :: Int
 
-                        --IO operation
+                        --plain action --IO operation
                         putStrLn ("Is it more than "
-                                 ++ show try
-                                 ++ " ? (True/False)")
-                        --IO action output (String) gets assigned to isGreater
-                        isGreater <- getLine
+                                 ++ (show try :: String)
+                                 ++ (" ? (True/False)")::String) :: IO ()
+                        --pure bind --IO action output (String) gets assigned to isGreater
                         
-                        --IF block
+                        isGreater <- getLine :: IO String --isGreater is String
+                        
+                        --plain action --IF block
                         if (read isGreater)
-                             --IO action
+                             --Bind--IO action
                              then loop (try + 1) b
-                             --IO action
+                             --Bind--IO action
                              else loop a       try         
                         -- the second do block ends
                         
-    --funktion call that returns type IO () and then that gets executed
+    --Bind --funktion call that returns type IO () and then that gets executed
     loop 1 10
-    --IO operation that writes a String to Console
-    putStrLn "Thanks for playing"
+    --Plain action --IO operation that writes a String to Console
+    putStrLn "Thanks for playing" :: IO ()
     --Do block ends here
