8172fe23bc200a2c9cad40edf1b04d71da508f57
EskoJTH <esjutaha@student.jyu.fi>
2017-04-04 14:59:08 +0200

    paljon kivasti opittu listää tietoverkkojuttuja.

---
+++ Tietoverkot/WebbiKaiku/WebbiKaiku/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbiKaiku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket palvelin = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 25000);
+            palvelin.Bind(iep);
+            palvelin.Listen(2);
+            while (true)
+            {
+                Socket asiakas = palvelin.Accept();
+                IPEndPoint a = (IPEndPoint)asiakas.RemoteEndPoint;
+                Console.WriteLine("Asiakas osoitteesta {0} portista {25000}" +  a.Address + a.Port);
+                //vastaanota
+                string viesti = "";
+                while (!viesti.Equals("exit"))
+                {
+                    byte[] rec = new byte[128];
+                    int paljon = asiakas.Receive(rec);
+                    viesti =Encoding.ASCII.GetString(rec, 0, paljon);
+                    asiakas.Send(Encoding.ASCII.GetBytes("viestejä viestejä viestejä ja ääkkösiä:  " +viesti+ "\n"));
+                }
+                //lähetä
+                asiakas.Send(Encoding.ASCII.GetBytes(viesti));
+
+                asiakas.Close();
+            }
+
+            Console.ReadKey();
+            palvelin.Close();
+        }
+    }
+}
---
+++ Tietoverkot/kaikua/kaikua/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaikua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String server = "localhost";
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            s.Connect(server,25000);
+
+            string message = "";
+            byte[] snd = Encoding.ASCII.GetBytes(message);
+            string sivu;
+            int paljon;
+            byte[] rec;
+            while (!message.Equals("exit"))
+            {
+                message = Console.ReadLine();
+                snd = Encoding.ASCII.GetBytes(message);
+                s.Send(snd);
+                paljon = 0;
+                rec = new byte[256];
+                paljon = s.Receive(rec);
+                sivu = Encoding.ASCII.GetString(rec, 0, paljon);
+                Console.Write(sivu);
+            }
+            /*paljon = 0;
+            rec = new byte[256];
+            snd = Encoding.ASCII.GetBytes(message);
+            sivu = Encoding.ASCII.GetString(rec, 0, paljon);*/
+            s.Close();
+        }
+    }
+}
