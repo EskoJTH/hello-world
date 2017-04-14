9dcd3e96263cbba709eeaf99d4ea6ea66ce71b48
EskoJTH <Esjutaha@student.jyu.fi>
2017-04-14 03:16:22 +0300

    tietoverkot webbihommia

---
+++ tietoverkot/Client/MyClient/MyClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect("localhost",25000);
+            String snd = "GET / HTTP/1.1\nHost: localhost\r\n\r\n";
+            byte[] buffer = Encoding.ASCII.GetBytes(snd);
+            s.Send(buffer);
+            String sivu = "";
+            int count;
+            do
+            {
+                byte[] rec = new byte[1024 * 10000];
+                count = s.Receive(rec);
+                Console.Write("Tavuaja vastaanotettu: " + count + "\n");
+                sivu += Encoding.ASCII.GetString(rec, 0, count);
+            } while (count > 0);
+            Console.Write(sivu);
+            Console.ReadKey();
+
+            s.Close();
+        }
+    }
+}
