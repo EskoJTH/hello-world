5b05c21eada7a1c26b7fa2a3d686e4e4dcb2861b
EskoJTH <esjutaha@student.jyu.fi>
2017-09-12 14:37:39 +0300

    T3 valmis

--- Haskell/F2/T3.hs
+++ Haskell/F2/T3.hs
@@ -1,24 +1,10 @@
-import Data.Monoid
-import Data.Semigroup
-g :: (Foldable f, Monoid m) => ((a -> m) -> f a -> m)     ->     ((b -> a -> b) -> b -> [a] -> b)
-g :: (Foldable f, Monoid m) => ((a -> m)        -> f a -> m)     ->     ((b -> a -> b) -> b -> [a] -> b) 
-g =
+import Data.Monoid hiding (Endo)
+
 foldl :: (b -> a -> b) -> b -> [a] -> b
-foldl f e xs = g . foldMap () xs where f=(/a b->())
+foldl f e = g . foldMap (\a->MelkeinEndo (\b->f b a)) where
+ g=(\(MelkeinEndo fu)->(fu e))
 
---(.) :: (b -> c) -> (a -> b) -> a -> c
-
---Taika taitaa olla monoidissa.
---(Foldable f, Monoid m) => (a -> m) -> f a -> m
--- m =? (a -> m)  ----- b
-newtype Fun a b = Fun {getFun :: a->b}
-instance (Monoid b) => Monoid (Fun a b) where
-  mempty = Fun mempty
-  mappend (Fun c) (Fun d) = Fun (\x->(c x) Data.Monoid.<> (d x))
-
---Insaideri tietoa käytä endoa.
-
-newtype Endo a = Endo {appEndo :: a -> a}
-instance Monoid (Endo a) where
-  mempty = Endo (\a-> a)
-  mappend (Endo a) (Endo b)  = Endo (a . b)
+newtype MelkeinEndo a = MelkeinEndo {appMelkeinEndo :: a -> a}
+instance Monoid (MelkeinEndo a) where
+  mempty = MelkeinEndo (\a-> a)
+  mappend (MelkeinEndo a) (MelkeinEndo b)  = MelkeinEndo (b . a)
