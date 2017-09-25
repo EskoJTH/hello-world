cae1ab977a1a7b682ec39464477a60769139244e
EskoJTH <esjutaha@student.jyu.fi>
2017-09-25 23:19:41 +0300

    lisätty tynkä T3

---
+++ Haskell/F2/V3T3.hs
@@ -0,0 +1,9 @@
+--1.Produce n random numbers, where n is taken as an argument
+--En k‰sit‰ mist‰ randomius esimerkeiss‰ oikein kumpuaa. Tied‰n ett‰ k‰tet‰‰n jotain satunnasilukuja tuottavaa funktiota. En tied‰ mist‰ seedi alussa revit‰‰n? vissiin monad joka alkaa jollain purella ja sitten ent tii‰.
+
+--2.Roll a die until a six comes up, printing out the rolls and their numbers
+
+--3.Parse a comma-separated list of natural numbers (such as the familiar "0,1,1,3,5,8,13,21,34")
+--t‰m‰n pit‰isi onnistua ihan ok Applicativell‰? Koska luennot?
+
+--4.Look up two given words from a dictionary (of type Map String String) and return a result if both are found. Easy! Maybe?
