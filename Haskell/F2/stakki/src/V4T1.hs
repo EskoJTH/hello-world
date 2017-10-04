354468b343c3a364457b5e984a412852779bc8d4
EskoJTH <esjutaha@student.jyu.fi>
2017-10-04 15:52:13 +0300

    V4T1 loistavasti valmis!

--- Haskell/F2/stakki/src/V4T1.hs
+++ Haskell/F2/stakki/src/V4T1.hs
@@ -10,11 +10,17 @@ data Terminal next =
   deriving Functor
 
 slurpLine :: Free Terminal String --as the counterpart of getLine,
-slurpLine = Free (SlurpLine (pure)) --Kysy tästä!
+slurpLine = Free (SlurpLine (pure)) -- mistä tuo pure tulee? Tai taisin jo keksiä. --Kysy tästä!
 barfLine :: String -> Free Terminal () --as the counterpart of putStrLn and
-barfLine = _
+barfLine s = Free (BarfLine s (terminate))
 terminate :: Free Terminal () --as the counterpart of pure ()
-terminate = undefined
+terminate = Free Done
+
+kaikuupi = do
+  l <- slurpLine
+  barfLine "-----"
+  barfLine l
+  kaikuupi
 
 echo = do
   l <- getLine
@@ -23,7 +29,20 @@ echo = do
   echo
   
 interpretIO :: Free Terminal r -> IO () --to convert it back to the reference implementation and
-interpretIO = undefined
+interpretIO (Free thing) = case thing of
+  SlurpLine f -> do
+    a <- getLine 
+    (interpretIO (f a))
+  BarfLine s n-> putStrLn s
+  Done -> return ()
+  
+   
 
 mockIO :: Free Terminal r -> [String] -> [String] --to emulate the implementation without actually running it.
-mockIO = undefined
+mockIO (Free thing) (x:commandLine) = case thing of
+  SlurpLine f -> mockIO (f x) commandLine
+  BarfLine s n-> mockIO n (s:(x:commandLine))
+  Done -> (x:commandLine)
+
+
+
