7e26f66e71a492e12639aaa4e2882ecb0ab73879
EskoJTH <esjutaha@student.jyu.fi>
2017-10-10 18:00:17 +0300

    Monadi siellä minne valo ei yleensä paista.

---
+++ Haskell/F2/stakki/src/V5T1.hs
@@ -0,0 +1,46 @@
+{-#language TypeSynonymInstances#-}
+{-#language FlexibleInstances#-}
+import Control.Monad.Writer.Lazy
+import Control.Monad.Reader
+{-
+data UFOLanguage next =
+  Move Point next |
+  Relax next |
+  ScanPlayer (Point -> next) |
+  Shoot Point next
+-}
+
+playRound = do
+  relax
+  target <- scanTargets
+  shoot target
+  move (1,1)
+
+  
+class Monad m => UFOLang m where
+  move  :: (Int,Int) -> m() 
+  relax :: m()
+  scanTargets :: m(Int,Int) 
+  shoot :: (Int,Int) -> m()
+  
+type T = ReaderT PeliTila (Writer String) 
+
+type PeliTila = [String]
+
+instance UFOLang T where
+  -- relax :: m()
+  relax = return ()
+  move (a,b) = do
+    p <- ask
+    tell $ ("liikuttiin pisteeseen (" ++ show a ++ "," ++ show b ++ ")")
+    return ()
+  scanTargets = do
+    p <- ask
+    tell $ ("scannattiin")
+    return (0,0)
+  shoot (a,b) = do
+    p <- ask
+    tell $ ("tulitettiin pistetta (" ++ show a ++ "," ++ show b ++ ")")
+
+writerScanner = do
+  tell (0,0)
