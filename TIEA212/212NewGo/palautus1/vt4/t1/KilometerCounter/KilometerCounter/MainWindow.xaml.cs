cd0fc5b2016abc1a6d86852efef0488a572a3c26
EskoJTH <esjutaha@student.jyu.fi>
2017-12-17 13:22:59 +0200

    lisäsin tiea 212 v4t1

---
+++ TIEA212/212NewGo/palautus1/vt4/t1/KilometerCounter/KilometerCounter/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KilometerCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void NewCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        public void NewCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Treeni_WPF_komponent.UserControl1 componenet = new Treeni_WPF_komponent.UserControl1();
+            mainPanel.Children.Add(componenet);
+        }
+
+        public void CloseCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        public void CloseCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ TIEA212/212NewGo/palautus1/vt4/t1/KilometerCounter/Treeni-WPF-komponent/UserControl1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Treeni_WPF_komponent
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+
+        public double Km { get; set; }
+        public double H { get; set; }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            SpeedLabel.Content = Math.Round((Km / H), 2);
+        }
+
+        private void Count(object sender, RoutedEventArgs e)
+        {
+            SpeedLabel.Content = Math.Round((Km / H), 2);
+        }
+
+    }
+}
+//<Treeni_WPF_komponent:UserControl1 HorizontalAlignment="Left" Height="Auto" Margin="5,5,5,5" VerticalAlignment="Top" Width="Auto"/>
